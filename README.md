1ac690e7b9cfdabf96cfd3447b52228a0d9e016e
Greg Glockner <greg@glockners.net>
2019-04-09 11:32:57 -0700

    Improve directory handling; documentation

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # pfsbackup
-Command-line backup for pfSense. Only saves changed backup files.
+Command-line backup for pfSense. This simple bash script copies the current configuration
+file from a remote pfSense server, and saves that configuration locally. Tested on
+macOS and Linux with pfSense 2.4.
 
 ## Getting started
 
+  1. Configure pfSense for ssh access via public key:
+    1. Go to System > Advanced > Admin Access, check Enable Secure Shell and click Save
+    1. Go to System > User Manager, edit the admin user and paste the public ssh key
   1. Edit values in pfs.sample.conf and save as pfs.conf
-  1. Upload a SSH public key to the admin user or the user that will run scp
-  1. Run the pfsbackup.sh script
+  1. Run the pfsbackup.sh script manually or schedule it to run via crontab
 
-## TODO
+## Hardening your pfSense configuration
 
-- Notify when backup fails
+  1. In pfsense:
+     1. Create a group called "scp" with privileges "User - System: Copy files (scp)"
+     1. Create a user called "backup" and add it to the "scp" group
+     1. Add the public key for the backup user
+     1. Disable password authentication for ssh
+  1. Change pfs.conf to use the backup user
--- pfsbackup.sh
+++ pfsbackup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Read backup configuration
-source pfs.conf
+# Read backup configuration, h/t https://stackoverflow.com/a/246128/446610
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${DIR}/pfs.conf
 
 PREFIX="config-${PFS_HOST}-"
 
@@ -12,28 +13,28 @@ scp -q ${PFS_USER}@${PFS_HOST}:/conf/config.xml ${NEW_CONF}
 # Only save if the configuration changed
 OLD_CONF=${BACKUP_DIR}/${PREFIX}latest.xml
 if [ -L ${OLD_CONF} ]; then
-	cmp -s ${OLD_CONF} ${NEW_CONF}
-	if [ $? -ne 0 ]; then
-		UPDATE=1
-	else
-		UPDATE=0
-	fi
+    cmp -s ${OLD_CONF} ${NEW_CONF}
+    if [ $? -ne 0 ]; then
+        UPDATE=1
+    else
+        UPDATE=0
+    fi
 else
-	UPDATE=1
+    UPDATE=1
 fi
 
 if [ ${UPDATE} -ne 0 ]; then
-	rm -fr ${OLD_CONF}
-	# Cull old backups, based on https://superuser.com/a/554548/393413
-	ls -1t ${BACKUP_DIR}/${PREFIX}*.xml | (i=0; while read f; do
-	  if [ $i -lt ${BACKUP_COUNT} ]; then
-		((i++))
-		continue
-	  else
-		rm "$f"
-	  fi
-	done)
-	ln -s ${NEW_CONF} ${OLD_CONF}
+    rm -fr ${OLD_CONF}
+    # Cull old backups, based on https://superuser.com/a/554548/393413
+    ls -1t ${BACKUP_DIR}/${PREFIX}*.xml | (i=0; while read f; do
+      if [ $i -lt ${BACKUP_COUNT} ]; then
+        ((i++))
+        continue
+      else
+        rm "$f"
+      fi
+    done)
+    ln -s ${NEW_CONF} ${OLD_CONF}
 else
-	rm ${NEW_CONF}
+    rm ${NEW_CONF}
 fi
